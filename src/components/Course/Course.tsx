c61c0585312179cfccd1e8deea3217050eb62d34
Hien Nguyen Thi Ngoc <hienntn@younetgroup.com>
2023-11-28 16:33:25 +0700

    update title dashboard

--- src/components/Course/Course.tsx
+++ src/components/Course/Course.tsx
@@ -2,9 +2,7 @@ import React, { useEffect } from 'react';
 import CourseCard from './CourseCard';
 
 const Course = () => {
-  useEffect(() => {
-    document.title = 'Khóa học | ITUTC2';
-  }, []);
+  
   return (
     <div className="grid relative gap-10 xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 ">
       <CourseCard />
