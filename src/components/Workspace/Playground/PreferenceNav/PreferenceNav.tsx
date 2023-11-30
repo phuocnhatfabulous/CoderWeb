b99655a7e88ba2565b9a44011cb8a28faa9b0e23
phuocnhatfabulous <doanphuocnhat205@gmail.com>
2023-11-30 17:08:52 +0700

    Add Programming languages

--- src/components/Workspace/Playground/PreferenceNav/PreferenceNav.tsx
+++ src/components/Workspace/Playground/PreferenceNav/PreferenceNav.tsx
@@ -1,72 +1,77 @@
-import { useState, useEffect } from "react";
-import { AiOutlineFullscreen, AiOutlineFullscreenExit, AiOutlineSetting } from "react-icons/ai";
-import { ISettings } from "../Playground";
-import SettingsModal from "@/components/Modals/SettingsModal";
+import { useState, useEffect } from 'react';
+import { AiOutlineFullscreen, AiOutlineFullscreenExit, AiOutlineSetting } from 'react-icons/ai';
+import { ISettings } from '../Playground';
+import SettingsModal from '@/components/Modals/SettingsModal';
+import DropDown from '@/components/DropDown';
 
 type PreferenceNavProps = {
-	settings: ISettings;
-	setSettings: React.Dispatch<React.SetStateAction<ISettings>>;
+  settings: ISettings;
+  setSettings: React.Dispatch<React.SetStateAction<ISettings>>;
 };
 
 const PreferenceNav: React.FC<PreferenceNavProps> = ({ setSettings, settings }) => {
-	const [isFullScreen, setIsFullScreen] = useState(false);
+  const [isFullScreen, setIsFullScreen] = useState(false);
 
-	const handleFullScreen = () => {
-		if (isFullScreen) {
-			document.exitFullscreen();
-		} else {
-			document.documentElement.requestFullscreen();
-		}
-		setIsFullScreen(!isFullScreen);
-	};
+  const handleFullScreen = () => {
+    if (isFullScreen) {
+      document.exitFullscreen();
+    } else {
+      document.documentElement.requestFullscreen();
+    }
+    setIsFullScreen(!isFullScreen);
+  };
 
-	useEffect(() => {
-		function exitHandler(e: any) {
-			if (!document.fullscreenElement) {
-				setIsFullScreen(false);
-				return;
-			}
-			setIsFullScreen(true);
-		}
+  useEffect(() => {
+    function exitHandler(e: any) {
+      if (!document.fullscreenElement) {
+        setIsFullScreen(false);
+        return;
+      }
+      setIsFullScreen(true);
+    }
 
-		if (document.addEventListener) {
-			document.addEventListener("fullscreenchange", exitHandler);
-			document.addEventListener("webkitfullscreenchange", exitHandler);
-			document.addEventListener("mozfullscreenchange", exitHandler);
-			document.addEventListener("MSFullscreenChange", exitHandler);
-		}
-	}, [isFullScreen]);
+    if (document.addEventListener) {
+      document.addEventListener('fullscreenchange', exitHandler);
+      document.addEventListener('webkitfullscreenchange', exitHandler);
+      document.addEventListener('mozfullscreenchange', exitHandler);
+      document.addEventListener('MSFullscreenChange', exitHandler);
+    }
+  }, [isFullScreen]);
 
-	return (
-		<div className='flex items-center justify-between bg-dark-layer-2 h-11 w-full '>
-			<div className='flex items-center text-white'>
-				<button className='flex cursor-pointer items-center rounded focus:outline-none bg-dark-fill-3 text-dark-label-2 hover:bg-dark-fill-2  px-2 py-1.5 font-medium'>
-					<div className='flex items-center px-1'>
-						<div className='text-xs text-label-2 dark:text-dark-label-2'>JavaScript</div>
-					</div>
-				</button>
-			</div>
+  return (
+    <div className="mt-5 ">
+      <div className="flex h-11 w-full items-center justify-between bg-dark-layer-2 ">
+        
+          {/* <button className="flex cursor-pointer items-center rounded bg-dark-fill-3 px-2 py-1.5 font-medium  text-dark-label-2 hover:bg-dark-fill-2 focus:outline-none ">
+          <div className="">
+            <div className="text-label-2 text-xs dark:text-dark-label-2 ">
+              <DropDown />
+            </div>
+          </div>
+        </button> */}
+          <DropDown />
 
-			<div className='flex items-center m-2'>
-				<button
-					className='preferenceBtn group'
-					onClick={() => setSettings({ ...settings, settingsModalIsOpen: true })}
-				>
-					<div className='h-4 w-4 text-dark-gray-6 font-bold text-lg'>
-						<AiOutlineSetting />
-					</div>
-					<div className='preferenceBtn-tooltip'>Settings</div>
-				</button>
+        <div className="m-2 flex items-center">
+          <button
+            className="preferenceBtn group"
+            onClick={() => setSettings({ ...settings, settingsModalIsOpen: true })}
+          >
+            <div className="h-4 w-4 text-lg font-bold text-dark-gray-6">
+              <AiOutlineSetting />
+            </div>
+            <div className="preferenceBtn-tooltip">Settings</div>
+          </button>
 
-				<button className='preferenceBtn group' onClick={handleFullScreen}>
-					<div className='h-4 w-4 text-dark-gray-6 font-bold text-lg'>
-						{!isFullScreen ? <AiOutlineFullscreen /> : <AiOutlineFullscreenExit />}
-					</div>
-					<div className='preferenceBtn-tooltip'>Full Screen</div>
-				</button>
-			</div>
-			{settings.settingsModalIsOpen && <SettingsModal settings={settings} setSettings={setSettings} />}
-		</div>
-	);
+          <button className="preferenceBtn group" onClick={handleFullScreen}>
+            <div className="h-4 w-4 text-lg font-bold text-dark-gray-6">
+              {!isFullScreen ? <AiOutlineFullscreen /> : <AiOutlineFullscreenExit />}
+            </div>
+            <div className="preferenceBtn-tooltip">Full Screen</div>
+          </button>
+        </div>
+        {settings.settingsModalIsOpen && <SettingsModal settings={settings} setSettings={setSettings} />}
+      </div>
+    </div>
+  );
 };
 export default PreferenceNav;
