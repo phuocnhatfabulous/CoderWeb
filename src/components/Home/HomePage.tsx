85fa35ea4c124a10aa89ecdc2db5630f64787bda
hienntn <hienntn@younetgroup.com>
2023-12-12 10:10:59 +0700

    update: add feature contest and blogs part in dashboard

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Slider from './Slider'
 import Course from '../Course/Course'
 import News from './News'
+import Blogs from './Blogs'
+import FeatureContest from './FeatureContest'
 
 const HomePage = () => {
   return (
     <div className=' my-8 place-items-center space-y-8'>
-      <Slider newsPage={false} />
+      <Slider newsPage={false} blogPart={false}/>
       <h2 className="text-brand-orange">Chào mừng đến với <span className='text-xl m-1 font-bold'>ITUTC2 Coder</span></h2>
       <div className='space-y-6'>
         <h2 className="h2-title">Tin tức & thông báo</h2>
@@ -28,11 +30,13 @@ const HomePage = () => {
           </svg>
         </div>
       </div>
-      <div>
+      <div className='space-y-6'>
         <h2 className="h2-title">Cuộc thi nổi bật</h2>
+        <FeatureContest />
       </div>
-      <div>
-        <h2 className="h2-title">Sinh viên IT nói gì về  Bộ môn IT UTC2</h2>
+      <div className='space-y-6'>
+        <h2 className="h2-title">Sinh viên IT nói gì về Bộ môn IT UTC2</h2>
+        <Blogs />
       </div>
     </div>
   )
