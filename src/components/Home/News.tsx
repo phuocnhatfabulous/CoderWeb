ccc29d88961a9088ef2e110f047d6e9379b3aa12
hienntn <hienntn@younetgroup.com>
2023-12-12 10:10:59 +0700

    update: add feature contest and blogs part in dashboard

--- src/components/Home/News.tsx
+++ src/components/Home/News.tsx
@@ -5,7 +5,7 @@ import Slider from './Slider';
 const News = () => {
   return (
     <div className='flex xl:md:flex-row sm:flex-col gap-16 p-6 shadow-lg shadow-slate-400 rounded-xl'>
-      <div className='w-1/2'>
+      <div className='xl:lg:w-1/2 md:sm:w-full'>
         <a href="" className='h-full select-text'>
           <div className=' w-full h-full relative p-6 flex flex-col gap-4 select-text'>
             <Image src="/news-1.jpg" alt='Ngày nhà giáo Việt Nam 20/11/2023 tại UTC2' width={500} height={500} className='object-cover shrink-0 w-full h-3/4 rounded-xl' />
--- src/components/Home/Slider.tsx
+++ src/components/Home/Slider.tsx
@@ -22,91 +22,140 @@ export default function Slider({ newsPage, blogPart }) {
 
   return (
     <>
-      {newsPage ?
-        <Swiper
-          effect={'coverflow'}
-          grabCursor={true}
-          centeredSlides={true}
-          slidesPerView={'auto'}
+      {
+        newsPage ?
+          <Swiper
+            effect={'coverflow'}
+            grabCursor={true}
+            centeredSlides={true}
+            slidesPerView={'auto'}
 
-          coverflowEffect={{
-            rotate: 0,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows: true,
-            scale: 0.9
-          }}
-          spaceBetween={0}
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
-          }}
-          loop={true}
-          loopAdditionalSlides={1}
-          pagination={
-            { clickable: true }
-          }
-          modules={[EffectCoverflow, Pagination, Autoplay]}
-          className="swiper-news relative"
-        >
-          {
-            news.map((n) => {
-              return (
-                <SwiperSlide className="relative" key={n.id}>
-                  <Link className="backdrop-blur-sm" href={n.link}>
-                    <Image alt={n.title} src={n.thumbnail} width={500} height={500} className="w-full h-full" />
-                    <div className="absolute bottom-2 bg-slate-500/30 z-50 p-2 items-center drop-shadow-xl m-2 rounded-xl backdrop-blur-sm">
-                      <span className="text-white-blue"> {n.title} </span>
-                    </div>
-                  </Link>
-                </SwiperSlide>
-              )
-            })
-          }
-
-        </Swiper>
-        :
-        <Swiper
-          cssMode={true}
-          navigation={true}
-          mousewheel={true}
-          keyboard={true}
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
-          }}
-          pagination={{
-            clickable: true,
-          }}
-          modules={[Navigation, Pagination, Mousewheel, Keyboard, Autoplay]}
-          className="swiper-dash p-2 select-none w-2/3"
-        >
-          <SwiperSlide>
-            <Image
-              src="/banner-1.png"
-              alt="banner1"
-              width={990}
-              height={200}
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image
-              src="/banner-1.png"
-              alt="banner1"
-              width={990}
-              height={200}
-            />
-          </SwiperSlide>
-          <SwiperSlide>
-            <Image
-              src="/banner-1.png"
-              alt="banner1"
-              width={990}
-              height={200}
-            />
-          </SwiperSlide>
-        </Swiper>
+            coverflowEffect={{
+              rotate: 0,
+              stretch: 0,
+              depth: 100,
+              modifier: 1,
+              slideShadows: true,
+              scale: 0.9
+            }}
+            spaceBetween={0}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+            loop={true}
+            loopAdditionalSlides={1}
+            pagination={
+              { clickable: true }
+            }
+            modules={[EffectCoverflow, Pagination, Autoplay]}
+            className="swiper-news relative rounded-xl sm:w-full"
+          >
+            {
+              news.map((n) => {
+                const getShortenedName = () => {
+                  if (n?.title.length <= 135) {
+                    return n?.title;
+                  } else {
+                    return `${n?.title.substring(0, 135)} ...`;
+                  }
+                }
+                return (
+                  <SwiperSlide className="relative" key={n.id}>
+                    <Link className="backdrop-blur-sm" href={n.link}>
+                      <Image alt={n.title} src={n.thumbnail} width={500} height={500} className="w-full h-full rounded-xl" />
+                      <div className="absolute bottom-2 bg-slate-500/30 z-50 p-2 items-center drop-shadow-xl m-2 rounded-xl backdrop-blur-sm hover:underline-offset-1 hover:underline text-white-blue">
+                        <span className="text-white-blue sm:text-sm hover:underline-offset-1 hover:underline"> {getShortenedName()} </span>
+                      </div>
+                    </Link>
+                  </SwiperSlide>
+                )
+              })
+            }
+          </Swiper>
+          :
+          blogPart ?
+            <Swiper
+              cssMode={true}
+              navigation={true}
+              mousewheel={true}
+              keyboard={true}
+              autoplay={{
+                delay: 4500,
+                disableOnInteraction: false,
+              }}
+              pagination={{
+                clickable: true,
+              }}
+              modules={[Navigation, Pagination, Mousewheel, Keyboard, Autoplay]}
+              className="swiper-dash p-2 select-none w-2/3 rounded-xl"
+            >
+              <SwiperSlide>
+                <Image
+                  src="/banner-1.png"
+                  alt="banner1"
+                  width={990}
+                  height={200}
+                />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image
+                  src="/banner-1.png"
+                  alt="banner1"
+                  width={990}
+                  height={200}
+                />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image
+                  src="/banner-1.png"
+                  alt="banner1"
+                  width={990}
+                  height={200}
+                />
+              </SwiperSlide>
+            </Swiper >
+            :
+            <Swiper
+              cssMode={true}
+              navigation={true}
+              mousewheel={true}
+              keyboard={true}
+              autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+              }}
+              pagination={{
+                clickable: true,
+              }}
+              modules={[Navigation, Pagination, Mousewheel, Keyboard, Autoplay]}
+              className="swiper-dash p-2 select-none w-2/3 rounded-xl"
+            >
+              <SwiperSlide>
+                <Image
+                  src="/banner-1.png"
+                  alt="banner1"
+                  width={990}
+                  height={200}
+                />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image
+                  src="/banner-1.png"
+                  alt="banner1"
+                  width={990}
+                  height={200}
+                />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Image
+                  src="/banner-1.png"
+                  alt="banner1"
+                  width={990}
+                  height={200}
+                />
+              </SwiperSlide>
+            </Swiper>
       }
     </>
   );
