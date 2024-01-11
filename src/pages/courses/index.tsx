1f6d15c6c8fe0c3df283ebfbb484c5757c2a6a30
phuocnhatfabulous <doanphuocnhat205@gmail.com>
2024-01-11 07:03:18 +0700

    Policy for CoursePage

--- src/pages/courses/index.tsx
+++ src/pages/courses/index.tsx
@@ -9,16 +9,15 @@ import TeachCourse from '@/components/Course/TeachCourse';
 import Link from 'next/link';
 
 const CoursePage = () => {
+  const userRole = useSetUserRole();
 
-  const userRole = useSetUserRole()
-  {userRole == 'sinhvien' && (
+  return (
     <DefaultLayout title={'Khóa học | ITUTC2'}>
-      {userRole ? (
-        <div className=" my-8 place-content-center space-y-6 px-36">
+      {userRole === 'sinhvien' ? (
+        <div className="my-8 place-content-center space-y-6 px-36">
           <div className='flex flex-col space-y-2'>
             <h2 className='h2-title text-deep-blue text-left'> Khóa học của tôi</h2>
             <Course />
-
           </div>
           <div className='items-center w-full flex flex-row justify-center'>
             <Image alt='' src="/atom-react.png" width={32} height={32} className='animate-spin ' />
@@ -31,22 +30,22 @@ const CoursePage = () => {
             <Image alt='' src="/atom-react.png" width={32} height={32} className='animate-spin ' />
           </div>
         </div>
+      ) : userRole === 'giangvien' ? (
+        <div className="my-8 place-content-center space-y-6 px-36">
+          <div className='flex flex-col space-y-2'>
+            <TeachCourse />
+          </div>
+        </div>
       ) : (
         <div className='items-center justify-center w-full h-full flex'>
-          <p className='text-center font-bold text-dark-blue text-xl shadow-md'>Bạn không có quyền truy cập trang này. Quay lại <Link href='/' className='underline underline-offset-1 text-brand-orange'>Trang chủ</Link></p>
+          <p className='text-center font-bold text-dark-blue text-xl shadow-md'>
+            Bạn không có quyền truy cập trang này. Quay lại <Link href='/' className='underline underline-offset-1 text-brand-orange'>Trang chủ</Link>
+          </p>
         </div>
       )}
     </DefaultLayout>
-  )};
-  return (
-    <DefaultLayout title={'Khóa học | ITUTC2'}>
-       <div className=" my-8 place-content-center space-y-6 px-36">
-        <div className='flex flex-col space-y-2'>
-          <TeachCourse />
-        </div>
-      </div>
-    </DefaultLayout>
-  )
+  );
 };
+
 
 export default CoursePage;
