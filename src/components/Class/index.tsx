56dfd7748f378b7c20fe1b6ed829ba5601797b28
phuocnhatfabulous <doanphuocnhat205@gmail.com>
2024-01-15 09:49:40 +0700

    Updating

--- src/components/Class/index.tsx
+++ src/components/Class/index.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
 
 const Class = () => {
-  return (
-    <div>Class</div>
-  )
-}
+  const [countdown, setCountdown] = useState(10);
 
-export default Class
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountdown((prevCountdown) => prevCountdown - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <div className="container">
+      <div className="items-center justify-center flex w-full">
+        <Image src="/comingsoon.png" alt="Logo" width={500} height={100} />
+      </div>
+      <div className="heading">Coming Soon</div>
+      <div className="description">We are working hard to bring you an amazing website.</div>
+      <div className="timer">{countdown > 0 ? `Launching in ${countdown} seconds` : 'Launching soon!'}</div>
+    </div>
+  );
+};
+
+export default Class;
--- src/components/Practices/TeachPractice.tsx
+++ src/components/Practices/TeachPractice.tsx
@@ -1,102 +1,146 @@
 import React, { useState, useEffect } from 'react';
-import { GoPlus } from "react-icons/go";
+import { GoPlus } from 'react-icons/go';
 import { Modal } from './Modals/Modal';
 import CreatePractice from './CreatePractice';
 
 const columns = [
-  { label: 'Tiêu đề', key: 'title' },
-  { label: 'Độ khó', key: 'description' },
-  { label: 'Trạng thái', key: 'status' },
+    { label: 'Tiêu đề', key: 'title' },
+    { label: 'Độ khó', key: 'description' },
+    { label: 'Trạng thái', key: 'status' },
 ];
 
 const data = [
-  {
-    id: 1,
-    title: 'Two Sum',
-    description: 'Easy',
-    status: 'Mọi người',
-  }
+    {
+        id: 1,
+        title: 'Two Sum',
+        description: 'Easy',
+        status: 'Mọi người',
+    },
+    {
+        id: 2,
+        title: 'Valid Parentheses',
+        description: 'Easy',
+        status: 'Mọi người',
+    },
+    {
+        id: 3,
+        title: 'Reverse Linked List',
+        description: 'Hard',
+        status: 'Mọi người',
+    },
+    {
+        id: 4,
+        title: 'Search a 2D Matrix',
+        description: 'Medium',
+        status: 'Mọi người',
+    },
+    {
+        id: 5,
+        title: 'Longest Palindromic Substring',
+        description: 'Medium',
+        status: 'Mọi người',
+    },
+    {
+        id: 6,
+        title: 'Zigzag Conversion',
+        description: 'Medium',
+        status: 'Mọi người',
+    },
+    {
+        id: 7,
+        title: 'Reverse Integer',
+        description: 'Medium',
+        status: 'Mọi người',
+    },
+    {
+        id: 8,
+        title: 'String to Integer (atoi)',
+        description: 'Easy',
+        status: 'Mọi người',
+    },
 ];
 
 const TeachPractice = () => {
-  const [rows, setRows] = useState(data);
-  const [rowToEdit, setRowToEdit] = useState(null);
-  const [modalOpen, setModalOpen] = useState(false);
-  const [alertMessage, setAlertMessage] = useState('');
+    const [rows, setRows] = useState(data);
+    const [rowToEdit, setRowToEdit] = useState(null);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [alertMessage, setAlertMessage] = useState('');
 
-  const handleEdit = (idx) => {
-    setRowToEdit(idx);
-    setModalOpen(true);
-  };
+    const handleEdit = (idx) => {
+        setRowToEdit(idx);
+        setModalOpen(true);
+    };
 
-  const handleDelete = (id) => {
-    try {
-      setRows(rows.filter((_, idx) => idx !== id));
-      setAlertMessage('Xóa thành công!');
-    } catch (e) {
-      setAlertMessage(`Lỗi: ${e}`);
-    }
-  };
+    const handleDelete = (id) => {
+        try {
+            setRows(rows.filter((_, idx) => idx !== id));
+            setAlertMessage('Xóa thành công!');
+        } catch (e) {
+            setAlertMessage(`Lỗi: ${e}`);
+        }
+    };
 
-  const handleSubmit = (newRow) => {
-    setRows((prevRows) => {
-      if (rowToEdit === null) {
-        return [...prevRows, newRow];
-      } else {
-        return prevRows.map((currRow, idx) => (idx !== rowToEdit ? currRow : newRow));
-      }
-    });
+    const handleSubmit = (newRow) => {
+        setRows((prevRows) => {
+            if (rowToEdit === null) {
+                return [...prevRows, newRow];
+            } else {
+                return prevRows.map((currRow, idx) =>
+                    idx !== rowToEdit ? currRow : newRow
+                );
+            }
+        });
 
-    setAlertMessage('Lưu thành công!');
-  };
+        setAlertMessage('Lưu thành công!');
+    };
 
-  useEffect(() => {
-    if (alertMessage) {
-      const timerId = setTimeout(() => {
-        setAlertMessage('');
-      }, 1000);
+    useEffect(() => {
+        if (alertMessage) {
+            const timerId = setTimeout(() => {
+                setAlertMessage('');
+            }, 1000);
 
-      return () => clearTimeout(timerId);
-    }
-  }, [alertMessage]);
+            return () => clearTimeout(timerId);
+        }
+    }, [alertMessage]);
 
-  return (
-    <div>
-      <div className="flex">
-        <h1 className="text-3xl font-bold mb-4">Quản lý bài tập</h1>
-        <GoPlus
-          onClick={() => setModalOpen(true)}
-          className="flex bg-dark-green-s ml-auto hover:bg-old-green-s text-white rounded  w-8 h-8 cursor-pointer p-2"
-        ></GoPlus>
-      </div>
+    return (
+        <div>
+            <div className="flex">
+                <h1 className="text-3xl font-bold mb-4">Quản lý bài tập</h1>
+                <GoPlus
+                    onClick={() => setModalOpen(true)}
+                    className="flex bg-dark-green-s ml-auto hover:bg-old-green-s text-white rounded  w-8 h-8 cursor-pointer p-2"
+                ></GoPlus>
+            </div>
 
-      {alertMessage && (
-        <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative">
-          {alertMessage}
+            {alertMessage && (
+                <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative">
+                    {alertMessage}
+                </div>
+            )}
+
+            {modalOpen && (
+                <Modal
+                    closeModal={() => {
+                        setModalOpen(false);
+                        setRowToEdit(null);
+                        setAlertMessage('');
+                    }}
+                    onSubmit={handleSubmit}
+                    defaultValue={rowToEdit !== null && rows[rowToEdit]}
+                    isEditMode={rowToEdit !== null}
+                />
+            )}
+
+            <CreatePractice
+                columns={columns}
+                data={rows}
+                onEdit={handleEdit}
+                onDelete={handleDelete}
+            />
         </div>
-      )}
-
-      {modalOpen && (
-        <Modal
-          closeModal={() => {
-            setModalOpen(false);
-            setRowToEdit(null);
-            setAlertMessage('');
-          }}
-          onSubmit={handleSubmit}
-          defaultValue={rowToEdit !== null && rows[rowToEdit]}
-          isEditMode={rowToEdit !== null}
-        />
-      )}
-
-      <CreatePractice
-        columns={columns}
-        data={rows}
-        onEdit={handleEdit}
-        onDelete={handleDelete}
-      />
-    </div>
-  );
+    );
 };
 
 export default TeachPractice;
